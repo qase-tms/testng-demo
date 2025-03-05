c22c2d9d9db9b84b4365b679d639bd03f893f9c1
Dmitrii Gridnev <dgridnev@qase.io>
2025-03-05 15:34:36 +0100

    Updated project

---
+++ src/test/java/com/example/utils/CustomDriverManager.java
@@ -0,0 +1,26 @@
+package com.example.utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CustomDriverManager {
+    private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    public static WebDriver getDriver() {
+        if (driver.get() == null) {
+            WebDriverManager.chromedriver().setup();
+            WebDriver drv = new ChromeDriver();
+            drv.manage().window().maximize();
+            driver.set(drv);
+        }
+        return driver.get();
+    }
+
+    public static void quitDriver() {
+        if (driver.get() != null) {
+            driver.get().quit();
+            driver.remove();
+        }
+    }
+}
